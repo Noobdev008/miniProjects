8308924caa13f11f06b3d504bcc757ee4adaa577
Shubham Srivastava <86356397+Noobdev008@users.noreply.github.com>
2022-02-02 19:21:07 +0530

    Using material UI

--- increment-decrement/src/App.js
+++ increment-decrement/src/App.js
@@ -1,33 +1,38 @@
 import React, { useState } from 'react';
 import './App.css';
+import { Button } from '@material-ui/core/';
+// import Stack from '@mui/material/Stack';
+import AddIcon from '@material-ui/icons/Add';
+import DeleteIcon from '@material-ui/icons/Delete'
+
 
 function App() {
-   const [num, setNum] = useState(0);
+  const [num, setNum] = useState(0);
 
-   const increment = () =>{
-     setNum(num+1)
-   }
-   const decrement = () =>{
-     if(num>0){
-    setNum(num-1)
-     }else{
-       alert("Limit Reached")
-       setNum(0)
-     }
+  const increment = () => {
+    setNum(num + 1)
+  }
+  const decrement = () => {
+    if (num > 0) {
+      setNum(num - 1)
+    } else {
+      alert("Limit Reached")
+      setNum(0)
+    }
   }
 
 
   return (
     <>
-    <div className='main_div'>
+      <div className='main_div'>
         <div className='center_div'>
           <h1>{num}</h1>
           <div className='btn_div'>
-             <button onClick={increment}>Increment</button>
-             <button onClick={decrement}>Decrement</button>
+            <Button onClick={increment} className='btn_green'><AddIcon /></Button>
+            <Button onClick={decrement} className='btn_red'><DeleteIcon /></Button>
           </div>
         </div>
-    </div>
+      </div>
     </>
   );
 }
